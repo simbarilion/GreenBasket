f51e851fe3cc729fbe43d3be42e434cccd8ae7ab
nadezhda <nadezhda13popova@gmail.com>
2026-04-27 03:20:06 +0700

    modified models in catalog app and created serializers

--- catalog/models.py
+++ catalog/models.py
@@ -1,8 +1,28 @@
+import uuid
+
 from django.core.validators import MinValueValidator
 from django.db import models
+from django.utils.text import slugify
+
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFill
 
 
-class Category(models.Model):
+class SlugMixin(models.Model):
+    """Универсальная модель-миксин для создания slug"""
+
+    slug = models.SlugField(unique=True, db_index=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = f"{slugify(self.name)}-{uuid.uuid4().hex[:6]}"
+        super().save(*args, **kwargs)
+
+
+class Category(SlugMixin):
     """
     Модель категории товаров.
     Может содержать подкатегории товаров и товары.
@@ -10,7 +30,6 @@ class Category(models.Model):
     """
 
     name = models.CharField(max_length=255)
-    slug = models.SlugField(unique=True)
     image = models.ImageField(upload_to="categories/")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -24,7 +43,7 @@ class Category(models.Model):
         ordering = ["name"]
 
 
-class SubCategory(models.Model):
+class SubCategory(SlugMixin):
     """
     Модель подкатегории товаров.
     Связана с категорией товаров. Может содержать товары.
@@ -33,7 +52,6 @@ class SubCategory(models.Model):
 
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="subcategories")
     name = models.CharField(max_length=255)
-    slug = models.SlugField()
     image = models.ImageField(upload_to="subcategories/")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -45,10 +63,9 @@ class SubCategory(models.Model):
         verbose_name = "Подкатегория"
         verbose_name_plural = "Подкатегории"
         ordering = ["name"]
-        unique_together = ("category", "slug")
 
 
-class Product(models.Model):
+class Product(SlugMixin):
     """
     Модель товара.
     Связана с подкатегорией товаров.
@@ -57,8 +74,28 @@ class Product(models.Model):
 
     subcategory = models.ForeignKey(SubCategory, on_delete=models.CASCADE, related_name="products")
     name = models.CharField(max_length=255)
-    slug = models.SlugField(unique=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
+    image = models.ImageField(upload_to="products/", default="products/images/default.png")
+    image_small = ImageSpecField(
+        source="image",
+        processors=[ResizeToFill(200, 200)],
+        format="JPEG",
+        options={"quality": 85},
+    )
+
+    image_medium = ImageSpecField(
+        source="image",
+        processors=[ResizeToFill(500, 500)],
+        format="JPEG",
+        options={"quality": 90},
+    )
+
+    image_large = ImageSpecField(
+        source="image",
+        processors=[ResizeToFill(1000, 1000)],
+        format="JPEG",
+        options={"quality": 95},
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -73,12 +110,3 @@ class Product(models.Model):
         verbose_name = "Товар"
         verbose_name_plural = "Товары"
         ordering = ["name"]
-
-
-class ProductImage(models.Model):
-    """Модель изображения товара"""
-
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="images")
-    image = models.ImageField(upload_to="products/")
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
